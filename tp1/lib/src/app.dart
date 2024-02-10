f6222b16b8c447605db2c05d7413f8430ded32a2
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-10 14:23:29 +0100

    Change app name + remove library

--- tp1/lib/src/app.dart
+++ tp1/lib/src/app.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:bookstore/src/screens/liked.dart';
+import 'package:media_app/src/screens/liked.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
