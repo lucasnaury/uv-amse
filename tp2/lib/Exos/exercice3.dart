65e611e9b396814805411faf15b2eeb92b5245fc
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-12 17:38:04 +0100

    Fix routing ex3

--- tp2/lib/Exos/exercice3.dart
+++ tp2/lib/Exos/exercice3.dart
@@ -12,45 +12,27 @@ class Exercice3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const Exercice3Example(),
-    );
-  }
-}
-
-class Exercice3Example extends StatelessWidget {
-  const Exercice3Example({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Liste Exercice')),
       body: ListView(
         children: <Widget>[
-          const Card(
-            child: ListTile(
-              title: Text('Liste déroulante des exerices allant de 1 à 7'),
-              dense: true,
-            ),
-          ),
           Card(
             child: ListTile(
               leading: const FlutterLogo(size: 56.0),
               title: const Text('Exercice 1'),
               subtitle: const Text('Here is a second line'),
               trailing: const Icon(Icons.play_arrow),
-              onTap: () => GoRouter.of(context).push(Exercice1),
+              onTap: () => GoRouter.of(context).push('/ex1'),
             ),
           ),
-          const Card(
+          Card(
             child: ListTile(
-              leading: FlutterLogo(size: 56.0),
-              title: Text('Exercice 2'),
-              subtitle:
-                  Text('A sufficiently long subtitle warrants three lines.'),
-              trailing: Icon(Icons.play_arrow),
-              isThreeLine: true,
+              leading: const FlutterLogo(size: 56.0),
+              title: const Text('Exercice 2'),
+              subtitle: const Text(
+                  'A sufficiently long subtitle warrants three lines.'),
+              trailing: const Icon(Icons.play_arrow),
+              onTap: () => GoRouter.of(context).push('/ex2'),
             ),
           ),
         ],
--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tp2/Exos/exercice1.dart';
 import 'package:tp2/Exos/exercice2.dart';
 import 'package:tp2/Exos/exercice3.dart';
@@ -13,9 +14,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'TP2',
-      home: Exercice3(),
+      debugShowCheckedModeBanner: false,
+      routerConfig: GoRouter(
+        initialLocation: '/',
+        routes: [
+          // top-level route
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const Exercice3(),
+          ),
+          GoRoute(
+            path: '/ex1',
+            builder: (context, state) => const Exercice1(),
+          ),
+          // Exercice 2
+          GoRoute(
+            path: '/ex2',
+            builder: (context, state) => const Exercice2(),
+          )
+        ],
+      ),
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
