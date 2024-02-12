9936cd305ad51d9d8fa10ae1ea783779092b217e
Niranjanfr <niranjan.kulkarni@etu.imt-nord-europe.fr>
2024-02-12 16:19:32 +0100

    resolution pb size

--- tp2/lib/Exos/exercice2.dart
+++ tp2/lib/Exos/exercice2.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Exercice1 extends StatefulWidget {
-  const Exercice1({super.key});
+class Exercice2 extends StatefulWidget {
+  const Exercice2({super.key});
 
   @override
-  State<Exercice1> createState() => _Exercice1State();
+  State<Exercice2> createState() => _Exercice2State();
 }
 
-class _Exercice1State extends State<Exercice1> {
+class _Exercice2State extends State<Exercice2> {
   double _currentSliderValue = 20;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(title: const Text('Slider')),
-      body: Column(
-        children: [
-          Image.asset('assets/imgs/taquin.jpg'),
-          Slider(
-            value: _currentSliderValue,
-            max: 100,
-            divisions: 5,
-            label: _currentSliderValue.round().toString(),
-            onChanged: (double value) {
-              setState(() {
-                _currentSliderValue = value;
-              });
-            },
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('assets/imgs/taquin.jpg'),
+              Slider(
+                value: _currentSliderValue,
+                max: 100,
+                divisions: 5,
+                label: _currentSliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                  });
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
