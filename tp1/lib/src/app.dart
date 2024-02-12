0d247d8f6de16f4b0bb0862385b1ce4ab57400f2
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-12 15:30:54 +0100

    Add SnackBar

--- tp1/lib/src/app.dart
+++ tp1/lib/src/app.dart
@@ -38,10 +38,30 @@ class Mediastore extends StatefulWidget {
 
 class _MediastoreState extends State<Mediastore> {
   //Toggle favorite function
-  void toggleFav(int id) {
+  void toggleFav(int id, context) {
     setState(() {
       widget.libraryInstance.toggleFavorite(id);
     });
+
+    bool liked = widget.libraryInstance.allMedias[id].liked;
+    // Show an alert dialog when liked
+    final snackBar = SnackBar(
+      duration: Duration(seconds: 2),
+      content: Text(liked ? "Ajouté aux favoris" : "Retiré des favoris"),
+      action: SnackBarAction(
+        label: 'Annuler',
+        onPressed: () {
+          setState(() {
+            //Revert toggle action when click on "annuler"
+            widget.libraryInstance.toggleFavorite(id);
+          });
+        },
+      ),
+    );
+
+    // Find the ScaffoldMessenger in the widget tree
+    // and use it to show a SnackBar
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   int mediaIndex = 0;
@@ -79,7 +99,8 @@ class _MediastoreState extends State<Mediastore> {
                     library: widget.libraryInstance,
                     onTap: (media) =>
                         GoRouter.of(context).push('/medias/info/${media.id}'),
-                    toggleFavCallback: toggleFav,
+                    // toggleFavCallback: toggleFav,
+                    toggleFavCallback: (id) => toggleFav(id, context),
                   );
                 },
                 routes: [
@@ -102,7 +123,8 @@ class _MediastoreState extends State<Mediastore> {
                   return LikedScreen(
                     child: MediaList(
                       medias: widget.libraryInstance.liked,
-                      toggleFavCallback: toggleFav,
+                      // toggleFavCallback: toggleFav,
+                      toggleFavCallback: (id) => toggleFav(id, context),
                       onTap: (media) =>
                           GoRouter.of(context).push('/liked/info/${media.id}'),
                     ),
