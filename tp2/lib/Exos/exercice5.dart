28f695fc9d86b809a9a6a8ff402a5a7be923cf7a
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-14 09:53:37 +0100

    Crop image to grid

--- tp2/lib/Exos/exercice5.dart
+++ tp2/lib/Exos/exercice5.dart
@@ -7,93 +7,120 @@ class Exercice5 extends StatefulWidget {
   State<Exercice5> createState() => _Exercice5State();
 }
 
+class Tile {
+  String imageURL;
+  Alignment alignment;
+  int gridSize;
+
+  Tile(
+      {required this.imageURL,
+      required this.gridSize,
+      required this.alignment});
+
+  //Create a cropped image tile
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: alignment,
+            widthFactor: 1.0 / gridSize,
+            heightFactor: 1.0 / gridSize,
+            child: Image.asset(imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _Exercice5State extends State<Exercice5> {
   int gridSize = 4;
+
+  List<Widget> tileWidgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    updateTiles();
+  }
+
+  void updateTiles() {
+    tileWidgets = [];
+
+    var offsetStep = 2 / (gridSize - 1);
+
+    for (int i = 0; i < gridSize; i++) {
+      for (int j = 0; j < gridSize; j++) {
+        tileWidgets.add(
+          createTileWidgetFrom(
+            Tile(
+              alignment: Alignment(-1 + offsetStep * j, -1 + offsetStep * i),
+              gridSize: gridSize,
+              imageURL: "assets/imgs/taquin.jpg",
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Exercice 5')),
       body: SafeArea(
-        child: Container(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              GridView.count(
-                primary: false,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-                shrinkWrap: true,
-                crossAxisCount: 3,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[100],
-                    child: const Center(child: Text("Tile 1")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[200],
-                    child: const Center(child: Text("Tile 2")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[300],
-                    child: const Center(child: Text("Tile 3")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[400],
-                    child: const Center(child: Text("Tile 4")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[500],
-                    child: const Center(child: Text("Tile 5")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[600],
-                    child: const Center(child: Text("Tile 6")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[700],
-                    child: const Center(child: Text("Tile 7")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[800],
-                    child: const Center(child: Text("Tile 8")),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[900],
-                    child: const Center(child: Text("Tile 9")),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 15),
-              Row(
+          child: Center(
+            child: Container(
+              alignment: Alignment.topCenter,
+              width: 350,
+              child: Column(
                 children: [
-                  const Text("Size : "),
-                  Expanded(
-                    child: Slider(
-                      value: gridSize.toDouble(),
-                      min: 2,
-                      max: 8,
-                      divisions: 6,
-                      label: gridSize.round().toString(),
-                      onChanged: (double value) {
-                        setState(() {
-                          gridSize = value.round();
-                        });
-                      },
-                    ),
+                  //Create a grid of the slider size
+                  GridView.count(
+                      primary: false,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5,
+                      shrinkWrap: true,
+                      crossAxisCount: gridSize,
+                      children: tileWidgets),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      const Text("Size : "),
+                      Expanded(
+                        child: Slider(
+                          value: gridSize.toDouble(),
+                          min: 2,
+                          max: 6,
+                          divisions: 4,
+                          label: gridSize.round().toString(),
+                          onChanged: (double value) {
+                            setState(() {
+                              gridSize = value.round();
+                              updateTiles();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
